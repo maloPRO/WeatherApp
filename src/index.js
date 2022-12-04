21775701fc564ed7b7259e0f5d754e07b5c490f3
maloPRO <malovagilbert@gmail.com>
2022-12-04 15:40:00 +0300

    added image to left card

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ const getWeather = (function () {
   const humidityValue = document.getElementById('humidity');
   const feelsLikeValue = document.getElementById('feelsLike');
   const pressureValue = document.getElementById('pressure');
-  const chanceValue = document.getElementById('chance');
+  const descValue = document.getElementById('desc');
   const conditionIcon = document.getElementById('conditionIcon');
   const windValue = document.getElementById('wind');
+  const centerImg = document.getElementById('centerImg');
 
   async function getWeatherToday(cityName = 'nairobi') {
     try {
@@ -28,15 +29,21 @@ const getWeather = (function () {
       const { pressure } = weatherData.main;
       const pic = weatherData.weather[0].icon;
       const wind = weatherData.wind.speed;
+      const desc = weatherData.weather[0].description;
+
+
+      console.log(weatherData);
 
       cityValue.textContent = city;
       conditionValue.textContent = condition;
       humidityValue.textContent = humidity;
-      feelsLikeValue.textContent = `${Math.ceil(feels_like)}${feelsLikeValue.textContent}`;
+      feelsLikeValue.textContent = Math.ceil(feels_like);
       pressureValue.textContent = pressure;
       tempValue.textContent = Math.round(temp);
       conditionIcon.src = `http://openweathermap.org/img/wn/${pic}@2x.png`;
+      centerImg.src = `http://openweathermap.org/img/wn/${pic}@2x.png`;
       windValue.textContent = `${wind}m/s`;
+      descValue.textContent = desc;
     } catch (error) {
       console.log('Error');
       console.error(error);
@@ -55,12 +62,11 @@ const handleDates = () => {
   const dateValue = document.getElementById('date');
   const timeValue = document.getElementById('time');
   const dateToday = format(new Date(), 'eeee, LLL d, yyyy');
-  const timeNow = format(new Date(), 'H:m:s');
+  const timeNow = format(new Date(), 'H:mm');
 
   dateValue.textContent = dateToday;
   timeValue.textContent = timeNow;
 };
 
 handleDates();
-getWeather.getWeatherToday('london');
-
+getWeather.getWeatherToday('nairobi');
